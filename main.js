313dd076bac80a3e6e443074966933f208422d69
Tony Nguyen <nguyen.tony1015@gmail.com>
2020-11-12 16:36:31 -0500

    uploading final hurrah

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 import ajax from "./modules/ajax.js"
 
-const mainContent = document.querySelector(".main-content") // grabbing the main content section
-document.body.append(mainContent) // adding this to the body
-const button = document.querySelector("#add")
 
-// possibly add a button here to append one at a time?
+const mainContent = document.querySelector(".main-content")
+const cardHolder = document.querySelector("#card-holder")
+const button = document.querySelector("#add")
+document.body.append(mainContent)
+
 let numArr = [];
-for (let i = 1; i < 21; i++) { // get 20 different values
+for (let i = 1; i < 31; i++) {
     let randomNum = Math.floor((Math.random() * 671)) // to generate a random number. 
     numArr.push(randomNum)
 }
-let url = `https://rickandmortyapi.com/api/character/${numArr}` // new url each time page refresh
+const url = `https://rickandmortyapi.com/api/character/${numArr}` // new url each time page refresh
 
 
 ajax(url, (character) => {
@@ -25,8 +26,16 @@ ajax(url, (character) => {
             // creating div for each char
             let div = document.createElement("div")
             div.className = "card"
-            mainContent.append(div)
+            // mainContent.append(div)
+            cardHolder.prepend(div)
 
+
+            //animation
+            div.classList.add("card", "faded-out")
+            setTimeout(() => { div.classList.remove("faded-out") }, 200)
+
+
+            // title
             let h2 = document.createElement("h2")
             h2.innerText = selectedChar.name
             div.append(h2)
@@ -37,76 +46,41 @@ ajax(url, (character) => {
             img.src = selectedChar.image
             div.append(img)
 
+
+            // more card info
+            let infoArr = ["status", "gender", "species"]
+            infoArr.forEach((attrib) => {
+                let h3 = document.createElement("h3")
+                h3.innerText = selectedChar[attrib]
+                div.append(h3)
+            })
+
+
+
+            // close button
             let closeButton = document.createElement("span")
             closeButton.innerText = "X"
             closeButton.className = "close-btn"
+            closeButton.addEventListener("click", (evt) => {
+                let parent = evt.target.parentNode;
+                parent.remove()
+            })
             div.append(closeButton)
-
         } else {
             null
         }
-
-        const btnClose = document.querySelector(".close-btn")
-        btnClose.addEventListener("click", (evt => {
-            evt.target.remove();
-        }))
     }
-
 
     button.addEventListener("click", generateCard)
 })
 
 
+// trying to play with animation
+
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 
+tl.to(".portal", { y: "0%", duration: 1, delay: 0.5 })
+tl.to(".intro", { y: "-100%", duration: 1, delay: 0.5 });
+tl.fromTo(".btn-container", { opacity: 0 }, { opacity: 1, duration: 1 });
 
-
-// callback function for the event listener 
-// const evtCallback = (evt) => {
-//     let randomNum = Math.floor((Math.random() * 671)) // to generate a random number. 
-//     let url = `https://rickandmortyapi.com/api/character/${randomNum}` // new url each time 
-
-
-//     ajax(url, (character) => {
-//         let parsedCharacter = JSON.parse(character);
-
-//         // create a div for each card. 
-//         let div = document.createElement("div")
-//         div.className = "card"
-//         div.innerText = parsedCharacter.name
-//         mainContent.append(div)
-
-//         // attaching an img
-//         let img = document.createElement("img")
-//         img.src = parsedCharacter.image
-//         div.append(img)
-//     })
-// }
-
-// button.addEventListener("click", evtCallback)
-
-
-
-
-
-// // this is accessing information from the API
-// ajax("https://rickandmortyapi.com/api/character/", (results) => {
-//     let parsedResults = JSON.parse(results);
-//     let resultsArr = parsedResults.results;
-
-
-
-//     // using a forEach to loop through the array
-//     resultsArr.forEach((result) => {
-
-//         // create a div for each card. 
-//         let div = document.createElement("div")
-//         div.className = "card"
-//         div.innerText = result.name
-//         mainContent.append(div)
-
-//         let img = document.createElement("img")
-//         img.src = result.image
-//         div.append(img)
-//     })
-// })
