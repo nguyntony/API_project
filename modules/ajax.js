ad6b2f06fa662d7250823e5e63366933501fbcd5
Tony Nguyen <nguyen.tony1015@gmail.com>
2020-11-13 20:58:00 -0500

    updating ajax

--- modules/ajax.js
+++ modules/ajax.js
@@ -3,7 +3,7 @@ const ajax = (url, callback, method = "GET") => {
     request.addEventListener("readystatechange", (evt) => {
         let req = evt.target;
         if (req.readyState !== 4) return;
-        if (req.status === 200 || req.status === 304) return callback(req.response) //this is all on one line
+        if (req.status === 200 || req.status === 304) return callback(req.response)
         callback(null, req.status)
     })
     request.open(method, url)
